c6f8894640e1b57583cc542962b3f3dcefa5da2d
emil <emst@eadania.dk>
2025-09-15 11:58:51 +0200

    Added character builder

--- Program.cs
+++ Program.cs
@@ -17,7 +17,46 @@
             switch (valg)
             {
                 case "1":
-                    Console.WriteLine("Starter spil ...");
+                    Console.WriteLine("\n=== CHARACTER BUILDER ===");
+
+                    Console.WriteLine("Vælg Faction:");
+                    Console.WriteLine("1) Horde");
+                    Console.WriteLine("2) Alliance");
+                    Console.Write("Valg: ");
+                    string factionValg = Console.ReadLine();
+                    string faction = factionValg == "1" ? "Horde"
+                                  : factionValg == "2" ? "Alliance"
+                                  : "Ukendt faction";
+
+                    Console.WriteLine("\nVælg Race:");
+                    Console.WriteLine("1) Orc");
+                    Console.WriteLine("2) Human");
+                    Console.WriteLine("3) Elf");
+                    Console.WriteLine("4) Dwarf");
+                    Console.Write("Valg: ");
+                    string raceValg = Console.ReadLine();
+                    string race = raceValg == "1" ? "Orc"
+                               : raceValg == "2" ? "Human"
+                               : raceValg == "3" ? "Elf"
+                               : raceValg == "4" ? "Dwarf"
+                               : "Ukendt race";
+
+                    Console.WriteLine("\nVælg Class:");
+                    Console.WriteLine("1) Warrior");
+                    Console.WriteLine("2) Mage");
+                    Console.WriteLine("3) Rogue");
+                    Console.Write("Valg: ");
+                    string classValg = Console.ReadLine();
+                    //Class is a protected name, uden the mortal kombat writing :)
+                    string klass = classValg == "1" ? "Warrior"
+                               : classValg == "2" ? "Mage"
+                               : classValg == "3" ? "Rogue"
+                               : "Ukendt class";
+
+                    Console.Write("\nSkriv et karakternavn: ");
+                    string navn = Console.ReadLine();
+
+                    Console.WriteLine($"\nHello \"{navn}\" you are a {faction} {race} {klass}.");
                     break;
                 case "2":
                     Console.WriteLine("Åbner indstillinger ...");
